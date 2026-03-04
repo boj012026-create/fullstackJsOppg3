d1cd699932dee94a45ac6abc6c62fb31d766efaf
Jan Olav Berg <boj012026@gmail.com>
2026-03-04 12:41:04 +0100

    fixed typo in comment task3

--- app.js
+++ app.js
@@ -178,7 +178,7 @@ function grammarNanny (strArr) {
     for( w of strArr) {
 	result.push(w.trim().toLowerCase());//remove space, all letters smal	
     }
-    return result.join(" ");//returns string array as a string
+    return result.join(" ");//returns result array as a string
     //return strArr.map(w => w.trim()).join(" ").toLowerCase();
 }
 
